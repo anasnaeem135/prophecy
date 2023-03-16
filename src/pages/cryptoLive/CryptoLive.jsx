44830cdb8675c9191e4b811bf8732349ed15d09a
anasnaeem135 <anasnaeem135@gmail.com>
2023-03-17 02:46:40 +0500

    cryptoLive

--- src/pages/cryptoLive/CryptoLive.jsx
+++ src/pages/cryptoLive/CryptoLive.jsx
@@ -5,12 +5,20 @@ import CardSlider from './components/cardSlider';
 import Header from 'components/header/header';
 import style from './CryptoLive.module.css';
 
+import { CheckBox } from '@mui/icons-material';
+
 const CryptoLive = () => {
     return (
         <>
             <Header />
             <div className={style.body}>
                 <CardSlider />
+
+                <br></br>
+
+                <div className={style.cardContainer}>
+                    <CheckBox defaultChecked />
+                </div>
             </div>
         </>
     );
